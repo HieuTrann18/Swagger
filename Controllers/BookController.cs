c8388d283d472bf257955c9656981224e772de43
TranTheHieu <hieu.tt.2455@aptechlearning.edu.vn>
2025-11-07 11:20:25 +0700

    Update BookController.cs

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using MyApi.Data;
-using MyApi.Models;
+using testapi.Data;
+using testapi.Models;
 
-namespace MyApi.Controllers
+namespace testapi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
@@ -19,20 +19,21 @@ namespace MyApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
         {
-            return await _context.Books.Include(b => b.Author).ToListAsync();
+            return await _context.Books
+                                 .Include(b => b.Author)
+                                 .ToListAsync();
         }
+
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Book>> GetBook(int id)
         {
             var book = await _context.Books
-                .Include(b => b.Author)
-                .FirstOrDefaultAsync(b => b.BookId == id);
+                                     .Include(b => b.Author)
+                                     .FirstOrDefaultAsync(b => b.BookId == id);
 
             if (book == null)
-            {
                 return NotFound();
-            }
 
             return book;
         }
@@ -41,16 +42,11 @@ namespace MyApi.Controllers
         [HttpPost]
         public async Task<ActionResult<Book>> CreateBook(Book book)
         {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
             _context.Books.Add(book);
             await _context.SaveChangesAsync();
 
-      
             return CreatedAtAction(nameof(GetBook), new { id = book.BookId }, book);
         }
-
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateBook(int id, Book book)
@@ -59,12 +55,22 @@ namespace MyApi.Controllers
                 return BadRequest();
 
             _context.Entry(book).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                    return NotFound();
+                else
+                    throw;
+            }
 
             return NoContent();
         }
 
-   
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteBook(int id)
         {
@@ -76,6 +82,11 @@ namespace MyApi.Controllers
             await _context.SaveChangesAsync();
 
             return NoContent();
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.BookId == id);
         }
     }
 }
