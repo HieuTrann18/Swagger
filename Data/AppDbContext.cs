5821a92735d9e6c4a2b1eba3efe09a14fdcf65d8
TranTheHieu <hieu.tt.2455@aptechlearning.edu.vn>
2025-11-07 11:09:08 +0700

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,22 +1,23 @@
-﻿namespace testapi.Data
+using Microsoft.EntityFrameworkCore;
+using testapi.Models;
+
+namespace testapi.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) { }
 
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
 
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Author>()
-            .HasMany(a => a.Books)
-            .WithOne(b => b.Author)
-            .HasForeignKey(b => b.AuthorId)
-            .OnDelete(DeleteBehavior.Cascade);
-
+                .HasMany(a => a.Books)
+                .WithOne(b => b.Author)
+                .HasForeignKey(b => b.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
 
             base.OnModelCreating(modelBuilder);
         }
