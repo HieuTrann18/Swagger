53b39223b3bbb536ae58ce7be6b52d3ade51f67a
TranTheHieu <hieu.tt.2455@aptechlearning.edu.vn>
2025-11-07 11:10:28 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using testapi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 🔹 Kết nối SQL Server (sửa chuỗi kết nối theo máy bạn)
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
