c74934e0ec73fe82f713117e7003dfbb527ab145
TranTheHieu <hieu.tt.2455@aptechlearning.edu.vn>
2025-11-07 11:32:17 +0700

    Update Book.cs

--- Models/Book.cs
+++ Models/Book.cs
@@ -4,30 +4,28 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyApi.Models
 {
-public class Book
-{
-public int BookId { get; set; }
-
-
-[Required]
-[StringLength(200)]
-public string Title { get; set; }
-
-
-[StringLength(100)]
-public string Genre { get; set; }
-
-
-[Range(1000, 2100)]
-public int PublicationYear { get; set; }
-
-
-// foreign key
-[Required]
-public int AuthorId { get; set; }
-
-
-// navigation
-public Author Author { get; set; }
-}
+    public class Book
+    {
+        public int BookId { get; set; }
+        
+        
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+        
+        
+        [StringLength(100)]
+        public string Genre { get; set; }
+        
+        
+        [Range(1000, 2100)]
+        public int PublicationYear { get; set; }
+        
+        
+        [Required]
+        public int AuthorId { get; set; }
+        
+        
+        public Author Author { get; set; }
+    }
 }
