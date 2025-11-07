90afbc66706f46f619e5aa553b3ea56b3c3d203b
TranTheHieu <hieu.tt.2455@aptechlearning.edu.vn>
2025-11-07 11:10:18 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using testapi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 🔹 Kết nối SQL Server (sửa chuỗi kết nối theo máy bạn)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy
+            .AllowAnyOrigin()   
+            .AllowAnyMethod()  
+            .AllowAnyHeader()); 
+});
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,6 +32,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
